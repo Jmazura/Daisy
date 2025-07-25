c5ff286bded873d370d05d71b38e2af76e7341e3
Yugiboyy <azurajhonmichael33@gmail.com>
2025-07-25 15:16:35 +0800

    Menu

--- Test_Folder/node.gd
+++ Test_Folder/node.gd
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input direction and handle the movemawasent/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("right", "left", "down", "up")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
