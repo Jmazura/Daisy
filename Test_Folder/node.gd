d3a40e70c0adf4ab3226de644dc821e6361a4c1c
Yugiboyy <azurajhonmichael33@gmail.com>
2025-07-25 14:21:31 +0800

    Animation Walk

--- Test_Folder/node.gd
+++ Test_Folder/node.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var anim:AnimationPlayer = get_node("CollisionShape3D/Catwalk Walk Forward 03/AnimationPlayer")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,10 +19,13 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("right", "left", "down", "up")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		anim.play("mixamo_com")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	if velocity == Vector3.ZERO:
+		anim.stop()
 	move_and_slide()
